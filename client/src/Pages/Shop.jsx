a0a5e1fdc36c47e7ac914bdc75acf233ac051903
Mehmed Ayt <Mehmed_ayt@abv.bg>
2024-08-18 18:24:30 +0300

    Add Popular and Item Component

--- client/src/Pages/Shop.jsx
+++ client/src/Pages/Shop.jsx
@@ -1,10 +1,11 @@
 import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/Popular/Popular";
 
 const Shop = () => {
     return (
         <div>
             <Hero/>
-
+            <Popular/>
         </div>
     );
 };
