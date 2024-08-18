0e1f56c0e79a186ae277ea72721e86833c764ff7
Mehmed Ayt <Mehmed_ayt@abv.bg>
2024-08-18 20:00:44 +0300

    Create NewsLetter Component

--- client/src/Pages/Shop.jsx
+++ client/src/Pages/Shop.jsx
@@ -1,5 +1,6 @@
 import Hero from "../Components/Hero/Hero";
 import NewCollections from "../Components/NewCollections/NewCollections";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
 import Offers from "../Components/Offers/Offers";
 import Popular from "../Components/Popular/Popular";
 
@@ -10,6 +11,7 @@ const Shop = () => {
             <Popular/>
             <Offers/>
             <NewCollections/>
+            <NewsLetter/>
         </div>
     );
 };
