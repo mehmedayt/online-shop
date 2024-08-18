083d298c78ee12bb0fcb5b77d93d99305eabb6f7
Mehmed Ayt <Mehmed_ayt@abv.bg>
2024-08-18 18:55:39 +0300

    Create Offers Component

--- client/src/Pages/Shop.jsx
+++ client/src/Pages/Shop.jsx
@@ -1,4 +1,5 @@
 import Hero from "../Components/Hero/Hero";
+import Offers from "../Components/Offers/Offers";
 import Popular from "../Components/Popular/Popular";
 
 const Shop = () => {
@@ -6,6 +7,7 @@ const Shop = () => {
         <div>
             <Hero/>
             <Popular/>
+            <Offers/>
         </div>
     );
 };
