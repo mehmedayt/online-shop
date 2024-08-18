bfbc50b65fe5fb2023e6ad14b9a505a6330e880b
Mehmed Ayt <Mehmed_ayt@abv.bg>
2024-08-18 19:23:36 +0300

    Create New Collection Component

--- client/src/Pages/Shop.jsx
+++ client/src/Pages/Shop.jsx
@@ -1,4 +1,5 @@
 import Hero from "../Components/Hero/Hero";
+import NewCollections from "../Components/NewCollections/NewCollections";
 import Offers from "../Components/Offers/Offers";
 import Popular from "../Components/Popular/Popular";
 
@@ -8,6 +9,7 @@ const Shop = () => {
             <Hero/>
             <Popular/>
             <Offers/>
+            <NewCollections/>
         </div>
     );
 };
