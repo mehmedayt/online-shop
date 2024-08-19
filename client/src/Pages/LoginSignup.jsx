d345a7ab8d4ad430403375de828fa57c737ab88f
Mehmed Ayt <Mehmed_ayt@abv.bg>
2024-08-19 12:31:40 +0300

    Update LoginSignup

--- client/src/Pages/LoginSignup.jsx
+++ client/src/Pages/LoginSignup.jsx
@@ -1,8 +1,22 @@
+import './CSS/LoginSignup.css';
+
 const LoginSignup = () => {
     return (
-        <div>
-<h1>Login page</h1>
-
+        <div className="loginsignup">
+            <div className="loginsignup-container">
+                <h1>Sign up</h1>
+                <div className="loginsignup-fields">
+                    <input type="text" placeholder="Your Name" />
+                    <input type="email" placeholder="Email Address" />
+                    <input type="password" placeholder="Password" />  
+                </div>
+                <button>Continue</button>
+                <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+                <div className="loginsignup-agree">
+                    <input type="checkbox" name="" id="" />
+                    <p>By continuing. I agree to the terms of use & privacy policy.</p>
+                </div>
+            </div>
         </div>
     );
 };
