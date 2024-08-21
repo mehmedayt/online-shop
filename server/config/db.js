24d31521f8fa96983e5af07a782ebe10ee3e8477
Mehmed Ayt <Mehmed_ayt@abv.bg>
2024-08-21 13:57:37 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI,  {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("MongoDB connected successfully");
     } catch (err) {
         console.error("MongoDB connection failed: ", err);
