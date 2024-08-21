f6125e2ea279c6d2d1c0d3899ab8a8e88ff73cf6
Mehmed Ayt <Mehmed_ayt@abv.bg>
2024-08-21 13:43:56 +0300

    Implemet List Product from DB

--- admin/src/Components/ListProduuct/ListProduct.jsx
+++ admin/src/Components/ListProduuct/ListProduct.jsx
@@ -1,8 +1,20 @@
-import './ListProduct.css';
+import { useEffect, useState } from 'react';
 import cross_icon from '../../assets/cross_icon.png';
+import './ListProduct.css';
 
 const ListProduct = () => {
 
+    const [allproducts,setAllproducts] = useState([]);
+
+    const fetchInfo = async () => {
+        await fetch('http://localhost:4000/allproducts')
+        .then((res)=>res.json())
+        .then((data)=>{setAllproducts(data);});
+    };
+
+    useEffect(()=>{
+        fetchInfo();
+    },[]);
 
     return (
         <div className='list-product'>
@@ -17,15 +29,16 @@ const ListProduct = () => {
             </div>
             <div className="list-allproducts">
                 <hr />
-                
-                    <div className="list-format-main list-format">
-                        <img src="" alt="" className="list-product-icon" />
-                        <p>Name</p>
-                        <p>Old Price</p>
-                        <p>New Price</p>
-                        <p>Category</p>
+                {allproducts.map((product,i)=>{
+                    return <div key={i} className="list-format-main list-format">
+                        <img src={product.image} alt="" className="list-product-icon" />
+                        <p>{product.name}</p>
+                        <p>${product.old_price}</p>
+                        <p>${product.new_price}</p>
+                        <p>{product.category}</p>
                         <img src={cross_icon} alt="" className="list-remove-icon" />
-                    </div>
+                    </div>;
+                })}
             </div>
         </div>
     );
