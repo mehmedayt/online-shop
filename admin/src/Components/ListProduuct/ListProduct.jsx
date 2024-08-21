491a9861a7920069049986249a286bbcce2bba7d
Mehmed Ayt <Mehmed_ayt@abv.bg>
2024-08-21 12:53:37 +0300

    Update List Product Component

--- admin/src/Components/ListProduuct/ListProduct.jsx
+++ admin/src/Components/ListProduuct/ListProduct.jsx
@@ -1,9 +1,32 @@
 import './ListProduct.css';
+import cross_icon from '../../assets/cross_icon.png';
 
 const ListProduct = () => {
+
+
     return (
-        <div>
-            <h1>List Product</h1>
+        <div className='list-product'>
+            <h1>All Products List</h1>
+            <div className="list-format-main">
+                <p>Products</p>
+                <p>Title</p>
+                <p>Old Price</p>
+                <p>New Price</p>
+                <p>Category</p>
+                <p>Remove</p>
+            </div>
+            <div className="list-allproducts">
+                <hr />
+                
+                    <div className="list-format-main list-format">
+                        <img src="" alt="" className="list-product-icon" />
+                        <p>Name</p>
+                        <p>Old Price</p>
+                        <p>New Price</p>
+                        <p>Category</p>
+                        <img src={cross_icon} alt="" className="list-remove-icon" />
+                    </div>
+            </div>
         </div>
     );
 };
