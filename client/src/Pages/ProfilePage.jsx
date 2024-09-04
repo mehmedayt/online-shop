84b460ff5cec15b7eb4f5c355151eae7d4ad6a9d
Mehmed Ayt <Mehmed_ayt@abv.bg>
2024-09-04 19:23:23 +0300

    Update ProfilePage component and styles

--- client/src/Pages/ProfilePage.jsx
+++ client/src/Pages/ProfilePage.jsx
@@ -1,9 +1,53 @@
-/* eslint-disable no-undef */
-/* eslint-disable react/prop-types */
+import './CSSPages/ProfilePage.css';
 
 const ProfilePage = () => {
+
+
     return (
-        <div>Profile Page</div>
+        <div className="profile">
+            <div className="profile-container">
+                <h1>Profile</h1>
+                <div className="profile-section">
+                    <h2>Change Password</h2>
+                    <input
+                        name="currentPassword"
+                        type="password"
+                        placeholder="Current Password"
+                    />
+                    <input
+                        name="newPassword"
+                        type="password"
+                        placeholder="New Password"
+                    />
+                    <input
+                        name="confirmPassword"
+                        type="password"
+                        placeholder="Confirm New Password"
+                    />
+                    <button>Update Password</button>
+                </div>
+
+                <div className="profile-section">
+                    <h2>Account Information</h2>
+                    <input
+                        name="username"
+                        type="text"
+                        placeholder="Change Username"
+                    />
+                    <p>Email: user@example.com</p>
+                    <button>Update Username</button>
+                </div>
+
+                <div className="profile-section">
+                    <h2>Danger Zone</h2>
+                    <button className="delete-account">
+                        Delete Account
+                    </button>
+                </div>
+            </div>
+
+          
+        </div>
     );
 };
 
