ef4d6b6ef987c4b759d73f9cb1386713ba56f9c7
Mehmed Ayt <Mehmed_ayt@abv.bg>
2024-10-16 23:34:58 +0300

    Add basic validations to User model

--- server/models/User.js
+++ server/models/User.js
@@ -3,16 +3,26 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, "Name is required"],
+        minlength: [3, "Name must be at least 3 characters long"],
+        maxlength: [50, "Name cannot exceed 50 characters"]
     },
     email: {
         type: String,
         unique: true,
-        required: true
+        required: [true, "Email is required"],
+        match: [/.+\@.+\..+/, "Please enter a valid email address"]  
     },
     password: {
         type: String,
-        required: true
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters long"],  
+        validate: {
+            validator: function(v) {
+                return /\d/.test(v);
+            },
+            message: "Password must contain at least one number"
+        }
     },
     cartData: {
         type: Object,
